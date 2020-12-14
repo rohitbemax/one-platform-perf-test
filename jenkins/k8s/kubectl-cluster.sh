50d8b1be9e49f7c1a36bebac317823cbd020fd91
rohit <rohit.bemax@gmail.com>
2020-12-14 16:39:25 +0530

    Correct kubectl syntax

--- jenkins/k8s/kubectl-cluster.sh
+++ jenkins/k8s/kubectl-cluster.sh
@@ -26,7 +26,6 @@ fi
 rm -rf ./GCP_KEY_FILE || echo "already deleted"
 
 if [ "$GCP" = "GCP" ] ; then
-  docker exec kubectl-support
   docker exec kubectl-support gcloud auth activate-service-account --key-file /opt/${GCP_KEY_FILE}
   docker exec kubectl-support gcloud config get-value core/account
   docker exec kubectl-support gcloud config set project ${GCP_K8_PROJECT}
